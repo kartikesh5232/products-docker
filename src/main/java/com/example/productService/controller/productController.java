b4bac116fc1f49130370804ce2dac228a3c0bd7a
Kartikesh <44187008+kartikesh5232@users.noreply.github.com>
2025-05-10 10:22:19 +0530

    4

---
+++ src/main/java/com/example/productService/controller/productController.java
@@ -0,0 +1,79 @@
+
+package com.example.productService.controller;
+
+import com.example.productService.entity.product;
+import com.example.productService.entity.productUploadResponse;
+
+import com.example.productService.service.productService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/image")
+public class productController {
+
+    @Autowired
+    private productService prodService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadImage(@RequestParam("file") MultipartFile file, @RequestParam("title") String title,
+                                         @RequestParam("description") String description, @RequestParam("price") int price,
+                                         @RequestParam("category") String category
+    ) throws IOException {
+
+        productUploadResponse response = prodService.uploadProduct(file,title, description, price, category);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(response);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test product";
+
+    }
+
+
+    @GetMapping("/{name}")
+    public ResponseEntity<?>  getImageByName(@PathVariable("name") String name){
+        byte[] image = prodService.getImage(name);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.valueOf("image/png"))
+                .body(image);
+    }
+
+    @GetMapping("/getproduct/{id}")
+    public ResponseEntity<?> getImageInfoByName(@PathVariable("id") Long id) {
+        System.out.println("getImageInfoByName");
+      product prod = prodService.getProducts( id);
+//        byte[] image = products.get(1).getImageData();
+//        return ResponseEntity.status(HttpStatus.OK)
+//              .contentType(MediaType.valueOf("image/png"))
+//                .body(image);
+
+        return ResponseEntity.status(HttpStatus.OK).body(prod);
+
+    }
+
+    @GetMapping("/getallproduct")
+    public ResponseEntity<?> getImageInfoByName() {
+        List<product> products = prodService.getAllProducts();
+//        byte[] image = products.get(1).getImageData();
+//        return ResponseEntity.status(HttpStatus.OK)
+//              .contentType(MediaType.valueOf("image/png"))
+//                .body(image);
+
+      return ResponseEntity.status(HttpStatus.OK).body(products);
+
+    }
+
+}
---
+++ src/main/java/com/example/productService/service/productService.java
@@ -0,0 +1,89 @@
+package com.example.productService.service;
+
+import com.example.productService.entity.product;
+import com.example.productService.entity.productUploadResponse;
+import com.example.productService.repository.productRepo;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class productService {
+
+    @Autowired
+    private productRepo productRepository;
+
+
+    public productUploadResponse uploadProduct(MultipartFile file, String title,
+                                               String description, int price, String category) throws IOException {
+
+        productRepository.save(product.builder()
+                .name(file.getOriginalFilename())
+                .type(file.getContentType())
+                .title(title)
+                .description(description)
+                .price(price)
+                .category(category)
+                .imageData(ImageUtil.compressImage(file.getBytes())).build());
+
+        return new productUploadResponse("Image uploaded successfully:");
+
+    }
+
+
+    @Transactional
+    public List<product> getAllProducts() {
+        List<product> products = productRepository.findAll();
+        return products.stream()
+                .map(prod -> {
+                    product newProd = new product();
+                    newProd.setId(prod.getId());
+                    newProd.setTitle(prod.getTitle());
+                    newProd.setName(prod.getName());
+                    newProd.setType(prod.getType());
+                    newProd.setDescription(prod.getDescription());
+                    newProd.setPrice(prod.getPrice());
+                    newProd.setCategory(prod.getCategory());
+                    newProd.setImageData(ImageUtil.decompressImage(prod.getImageData()));
+                    return newProd;
+                })
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public product getProducts(Long id) {
+       Optional<product> prod = productRepository.findById(id);
+
+
+                    product newProd = new product();
+                    newProd.setId(prod.get().getId());
+                    newProd.setTitle(prod.get().getTitle());
+                    newProd.setName(prod.get().getName());
+                    newProd.setType(prod.get().getType());
+                    newProd.setDescription(prod.get().getDescription());
+                    newProd.setPrice(prod.get().getPrice());
+                    newProd.setCategory(prod.get().getCategory());
+                    newProd.setImageData(ImageUtil.decompressImage(prod.get().getImageData()));
+                    return newProd;
+
+
+    }
+
+    @Transactional
+    public byte[] getImage(String name) {
+        Optional<product> dbImage = productRepository.findByName(name);
+
+        if (dbImage.isEmpty()) {
+            throw new RuntimeException("Product with name " + name + " not found");
+        }
+
+        return ImageUtil.decompressImage(dbImage.get().getImageData());
+    }
+
+}
